45bc12e92b16595a6a49dcb9aa5fd04b6406de1c
Arthur Lee <arthuralee@users.noreply.github.com>
2022-02-06 16:55:45 -0500

    test

--- .github/actions/comment-triggers/index.js
+++ .github/actions/comment-triggers/index.js
@@ -1,10 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const githubToken = core.getInput('githubToken');
+const octokit = github.getOctokit(githubToken);
+
 const { context } = github;
 
 async function run() {
 	console.warn(context);
+  const commentBody = context.payload.comment.body;
+  const prNumber = context.payload.issue.number;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: prNumber,
+    body: `You said: ${commentBody}`,
+  })
 }
 
 run().catch(error => {
